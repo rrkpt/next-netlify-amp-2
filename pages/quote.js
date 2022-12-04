3f2e6bfeeda833c6d8a4ea724601f9d5cc3cb48e
Roman Rakus <roman.rakus@karpaty.energy>
2022-12-04 22:30:31 +0200

    Added quote page

---
+++ pages/quote.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Quote() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Get Quote</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label>Your Name: <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
