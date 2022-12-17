2e5619660e0e9ecad73cb933b581f8aeb904394a
Roman Rakus <roman.rakus@karpaty.energy>
2022-12-17 14:04:09 +0200

    added monday submission

--- pages/api/mondayForm.js
+++ pages/api/mondayForm.js
@@ -11,6 +11,18 @@ export default function handler(req, res) {
         return res.status(400).json({ data: 'Phone not found' })
     }
 
+    const endpoint = 'https://karpaty-energy.monday.com/api/'
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE0MDM2MTY3NCwidWlkIjoyMzQ2MTQ4MywiaWFkIjoiMjAyMi0wMS0xMVQxOTowMzozNC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTUyODI3OSwicmduIjoidXNlMSJ9.BwcFQEMGgb0Wdwq5NtkyPIOmPJoBZkQZoPPUUlyvtP0'
+        },
+        body: body
+    }
+    fetch(endpoint, options)
+        .then((response) => console.log(response))
+
     // Found the name.
     // Sends a HTTP success code
     res.status(200).json({ data: `${body.name} ${body.phone}` })
