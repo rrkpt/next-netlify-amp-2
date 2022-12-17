dc3ed6095cdeb06abb5c483743e4c76fe2910506
Roman Rakus <roman.rakus@karpaty.energy>
2022-12-17 22:31:14 +0200

    WORKING submit form

--- pages/api/mondayForm.js
+++ pages/api/mondayForm.js
@@ -2,27 +2,29 @@ export default async function handler(req, res) {
     // Get data submitted in request's body.
     const body = req.body
 
-    // Optional logging to see the responses
-    // in the command line where next.js app is running.
-    console.log('body: ', body)
-
     if (!body.phone) {
         return res.status(400).json({ data: 'Phone not found' })
     }
 
-    const endpoint = 'https://api.monday.com/v2/'
-    const options = {
-        method: 'POST',
+    let query = "mutation ($itemName:String, $columnValues:JSON!) { create_item (board_id: 2439635456, item_name: $itemName, column_values: $columnValues) { id }}";
+    fetch("https://api.monday.com/v2", {
+        method: 'post',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE0MDM2MTY3NCwidWlkIjoyMzQ2MTQ4MywiaWFkIjoiMjAyMi0wMS0xMVQxOTowMzozNC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6OTUyODI3OSwicmduIjoidXNlMSJ9.BwcFQEMGgb0Wdwq5NtkyPIOmPJoBZkQZoPPUUlyvtP0'
         },
-        body: body
-    }
-    const response = await fetch(endpoint, options)
-    console.log(response.status)
+        body: JSON.stringify({
+            query: query,
+            variables: JSON.stringify({
+                itemName: body.name,
+                columnValues: "{\"phone_1\" : {\"phone\" : \"" + body.phone + "\"}}"
+            })
+        })
+    })
+        .then(createRes => createRes.json())
+        .then(createRes => console.log(JSON.stringify(createRes, null, 2)));
 
     // Found the name.
     // Sends a HTTP success code
-    res.status(200).json({ data: `${body.name} ${body.phone}` })
+    res.status(200)
 }
