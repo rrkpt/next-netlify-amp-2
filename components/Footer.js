bad204d084e1a76bfc23761e86ef5b51b1660d45
Roman Rakus <roman.rakus@karpaty.energy>
2023-01-09 19:28:08 +0200

    dummy phone number

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        08001234567
       </footer>
     </>
   )
